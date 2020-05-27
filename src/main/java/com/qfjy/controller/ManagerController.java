458c315950042bfb148edf76b80161aab5124ed4
weixin_global <315759265@qq.com>
2020-05-27 11:10:26 +0800

    项目经理-->增加代码修改

---
+++ src/main/java/com/qfjy/controller/ManagerController.java
@@ -0,0 +1,12 @@
+package com.qfjy.controller;
+
+/**
+ * @Classname ManagerController
+ * @Author guoweixin
+ * @Description TODO
+ * @Date 2020/5/27 11:09
+ * @Created by Administrator
+ */
+public class ManagerController {
+    //项目经理
+}
