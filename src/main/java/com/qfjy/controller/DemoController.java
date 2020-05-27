289f5588996ce85bd1515dc3131269a9695e1a3d
weixin_global <315759265@qq.com>
2020-05-27 10:29:06 +0800

    首次提交 ch2-demo项目

---
+++ src/main/java/com/qfjy/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.qfjy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname DemoController
+ * @Author guoweixin
+ * @Description TODO
+ * @Date 2020/5/27 10:24
+ * @Created by Administrator
+ */
+@RestController
+public class DemoController {
+    @RequestMapping("info")
+    public String info(){
+        return "Hello Git";
+    }
+}
